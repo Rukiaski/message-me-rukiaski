6dee79e70087417a11d2314e8fc752ba502efa71
Rukiaski <thamiresaraujoml@gmail.com>
2019-08-22 14:54:21 -0300

    Chatroom it's work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
 
+    before_action :logged_in_redirect, only: [:new, :create]
     def new
     end
     
@@ -20,4 +21,12 @@ class SessionsController < ApplicationController
         flash[:success] = "Successfully loged out"
         redirect_to root_path
     end
+
+    private
+        def logged_in_redirect
+            if logged_in?
+                flash[:error] = "You already logged in!"
+                redirect_to root_path
+            end
+        end
 end
